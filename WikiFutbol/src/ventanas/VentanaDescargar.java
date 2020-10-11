8f793080c215955e0ac99bdef43c2f6e1646c623
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-10-11 19:54:44 +0200

    jlist ""automatizado"" para visualizar las tablas descargables

--- WikiFutbol/src/ventanas/VentanaDescargar.java
+++ WikiFutbol/src/ventanas/VentanaDescargar.java
@@ -3,17 +3,20 @@ package ventanas;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 
 import database.AdvancedDb2CsvExporter;
+import database.DBManagerException;
 
 public class VentanaDescargar extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	JButton btn;
 	JList<Object> b;
+	DefaultListModel<?> listModel;
 
 	public VentanaDescargar() {
 
@@ -28,12 +31,30 @@ public class VentanaDescargar extends JFrame {
 		btn.setText("btn");
 		btn.setBounds(240, 250, 120, 30);
 
-		// String array to store weekdays
-		String tablas[] = { "usuario", "jugador" };
-
 		// create list
-		b = new JList<Object>(tablas);
+		b = new JList<Object>();
 		b.setBounds(10, 10, 200, 200);
+		String[] tablasProhibidas = { "usuario" }; // no dejamos descargar estas tablas por seguridad
+		DefaultListModel<Object> listModel = new DefaultListModel<Object>();
+		try {
+			for (int i = 0; i < database.DBManager.verTablas().size(); i++) {
+				for (int j = 0; j < tablasProhibidas.length; j++) {
+					try {
+						String tabla = database.DBManager.verTablas().get(i);
+						if (!tabla.equals(tablasProhibidas[j])) {
+							listModel.add(i, tabla);
+						}
+					} catch (DBManagerException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		} catch (DBManagerException e1) {
+			e1.printStackTrace();
+		}
+		b.setModel(listModel);
+		b.setBounds(10, 10, 200, 200);
+		// fin de list
 
 		add(btn);
 		add(b);
