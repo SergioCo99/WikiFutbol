40c2e6966c7e076f4da724061f145252b1b581ce
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-10-13 11:55:59 +0200

    metodo que ahora si funciona para no poder descargar unas tablas

--- src/ventanas/VentanaDescargar.java
+++ src/ventanas/VentanaDescargar.java
@@ -7,6 +7,7 @@ import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 
 import database.AdvancedDb2CsvExporter;
 import database.DBManagerException;
@@ -14,67 +15,74 @@ import database.DBManagerException;
 public class VentanaDescargar extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	JButton btn;
-	JList<Object> b;
+	JButton btnDescargar;
+	JList<Object> listaTablas;
 	DefaultListModel<?> listModel;
 
 	public VentanaDescargar() {
 
-		this.setTitle("VentanaAdmin1");
+		this.setTitle("VentanaDescargar");
 		this.setSize(600, 400);
 		this.setLayout(null);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 
-		btn = new JButton();
-		btn.setText("btn");
-		btn.setBounds(240, 250, 120, 30);
+		btnDescargar = new JButton();
+		btnDescargar.setText("Descargar");
+		btnDescargar.setBounds(240, 250, 120, 30);
 
 		// create list
-		b = new JList<Object>();
-		b.setBounds(10, 10, 200, 200);
+		listaTablas = new JList<Object>();
+		listaTablas.setBounds(10, 10, 200, 200);
 		String[] tablasProhibidas = { "usuario" }; // no dejamos descargar estas tablas por seguridad
 		DefaultListModel<Object> listModel = new DefaultListModel<Object>();
+
 		try {
 			for (int i = 0; i < database.DBManager.verTablas().size(); i++) {
-				for (int j = 0; j < tablasProhibidas.length; j++) {
-					try {
-						String tabla = database.DBManager.verTablas().get(i);
-						if (!tabla.equals(tablasProhibidas[j])) {
-							listModel.add(i, tabla);
-						}
-					} catch (DBManagerException e1) {
-						e1.printStackTrace();
+				String tabla = database.DBManager.verTablas().get(i);
+				listModel.add(i, tabla);
+			}
+			for (int j = 0; j < tablasProhibidas.length; j++) {
+				for (int i = 0; i < database.DBManager.verTablas().size(); i++) {
+					String tabla = database.DBManager.verTablas().get(i);
+					if (tablasProhibidas[j].equals(tabla)) {
+						listModel.remove(i);
 					}
 				}
 			}
 		} catch (DBManagerException e1) {
 			e1.printStackTrace();
 		}
-		b.setModel(listModel);
-		b.setBounds(10, 10, 200, 200);
+
+		listaTablas.setModel(listModel);
+		listaTablas.setBounds(10, 10, 200, 200);
 		// fin de list
 
-		add(btn);
-		add(b);
+		add(btnDescargar);
+		add(listaTablas);
 
-		btn.addActionListener(new ActionListener() {
+		btnDescargar.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("btn");
-
-				try {
-					AdvancedDb2CsvExporter.export("jugador");
-				} catch (Exception e1) {
-					e1.printStackTrace();
+				if (listaTablas.isSelectionEmpty() == false) {
+					try {
+						AdvancedDb2CsvExporter.export(listaTablas.getSelectedValue().toString());
+						JOptionPane.showMessageDialog(null, "Se ha descargado correctamente.");
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "No se ha descargado nada.", "Alert",
+							JOptionPane.WARNING_MESSAGE);
 				}
 			}
 		});
 
 	}
 
+	// este main es para pruebas, habria que quitarlo
 	public static void main(String[] args) {
 		VentanaDescargar VD = new VentanaDescargar();
 		VD.setVisible(true);
