84855cf3fe92729e2a318063288367bd2e04d572
SergioCo99 <sergiocogollos@opendeusto.es>
2020-10-22 19:40:31 +0200

    EmpleadoDeClub test

--- test/clases/EmpleadoClubTest.java
+++ test/clases/EmpleadoClubTest.java
@@ -11,11 +11,13 @@ public class EmpleadoClubTest {
 	
 	@Before
 	public void setUp() {
+		
 		ect = new EmpleadoDeClub(1 , "Koikili", 1980, 1, 1);
 	}
 	
 	@Test
 	public void testId() {
+		
 		ect.setId(1);
 		assertEquals(1, ect.getId());
 	}
@@ -23,30 +25,35 @@ public class EmpleadoClubTest {
 	
 	@Test
 	public void testNombre() {
+		
 		ect.setNombre("Koikili");
 		assertEquals("Koikili", ect.getNombre());
 	}
 	
 	@Test
 	public void testAnyoNacimiento() {
+		
 		ect.setAnyoNacimiento(1980);
 		assertEquals(1980, ect.getAnyoNacimiento());
 	}
 	
 	@Test
 	public void testClub() {
+		
 		ect.setClub(1);
 		assertEquals(1 , ect.getClub());
 	}
 	
 	@Test
 	public void testCiudad() {
+		
 		ect.setCiudad(1);
 		assertEquals(1, ect.getCiudad());
 	}
 	
 	@Test
 	public void metodoToString() {
+		
 		ect.setId(1);
 		ect.setNombre("Koikili");
 		ect.setAnyoNacimiento(1980);
@@ -54,6 +61,7 @@ public class EmpleadoClubTest {
 		ect.setCiudad(1);
 		
 		System.out.println(ect.toString());
+		
 		assertEquals(
 				"EmpleadoDeClub [id=1, nombre=Koikili, anyoNacimiento=1980, club=1, ciudad=1]",
 				ect.toString());
