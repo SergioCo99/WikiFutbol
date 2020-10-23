14f3d9ec8ee883cedf654df2b08781cb48e71fe2
SergioCo99 <sergiocogollos@opendeusto.es>
2020-10-23 09:59:22 +0200

    Test Feedback + TOY

---
+++ test/clases/FeedbackTest.java
@@ -0,0 +1,65 @@
+package clases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import clases.Feedback.Recomendacion;
+import clases.Feedback.Valoracion;
+
+public class FeedbackTest {
+
+	private Feedback f;
+	
+	@Before
+	public void setUp() {
+		f = new Feedback(1, "a", Valoracion.CINCO, Recomendacion.si, "Correcto");
+	}
+
+	@Test
+	public void testId() {
+		f.setId(1);
+		assertEquals(1, f.getId());
+	}
+	
+	@Test
+	public void testUsuario() {
+		f.setUsuario("a");
+		assertEquals("a", f.getUsuario());
+	}
+	
+	@Test
+	public void testValoracion() {
+		f.setValoracion(Valoracion.CINCO);
+		assertEquals(Valoracion.CINCO, f.getValoracion());
+	}
+	
+	@Test
+	public void testRecomendacion() {
+		f.setRecomendacion(Recomendacion.si);
+		assertEquals(Recomendacion.si, f.getRecomendacion());
+	}
+	
+	@Test
+	public void testOpinion() {
+		f.setOpinion("Correcto");
+		assertEquals("Correcto", f.getOpinion());
+	}
+	
+	@Test
+	public void metodoToString() {
+
+		f.setId(1);
+		f.setUsuario("a");
+		f.setValoracion(Valoracion.CINCO);
+		f.setRecomendacion(Recomendacion.si);
+		f.setOpinion("Correcto");
+
+		System.out.println(f.toString());
+
+		assertEquals(
+				"Feedback [id=1, usuario=a, valoracion=CINCO, recomendacion=si, opinion=Correcto]",
+				f.toString());
+	}
+}
