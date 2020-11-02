c18a8cb3508c449c77680b21ae3e07b8e8185fc2
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-11-02 19:55:14 +0100

    mainwikifutboltest, por ahora vacio

---
+++ test/mainPackage/MainWikiFutbolTest.java
@@ -0,0 +1,19 @@
+package mainPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainWikiFutbolTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
