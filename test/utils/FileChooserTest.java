b4eae81a4e3cabf007d15ce6e2c6762316b7d843
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-11-02 16:42:48 +0100

    test de utils.filechooser.choose

--- test/utils/FileChooserTest.java
+++ test/utils/FileChooserTest.java
@@ -7,13 +7,26 @@ import org.junit.Test;
 
 public class FileChooserTest {
 
+	static FileChooser fc = new FileChooser();
+
 	@Before
 	public void setUp() throws Exception {
+		fc = new FileChooser();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		// Hay que seleccionar el archivo del String actual en concreto
+		String finalPath = utils.FileChooser.Choose();
+		String actual = System.getProperty("user.home") + "\\git\\WikiFutbol\\resources\\wf.png";
+
+		assertEquals(finalPath, actual);
+
+		// Esta vez no se selecciona nada, es decir, boton cancelar o X
+		String finalPath2 = utils.FileChooser.Choose();
+		String actual2 = "";
+
+		assertEquals(finalPath2, actual2);
 	}
 
 }
