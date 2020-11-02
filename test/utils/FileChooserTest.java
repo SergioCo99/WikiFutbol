feac07ba37fd5dfc6bcaebba553d6cd8f417d97c
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-11-02 16:19:01 +0100

    empiezo test del package utils

---
+++ test/utils/FileChooserTest.java
@@ -0,0 +1,19 @@
+package utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FileChooserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
