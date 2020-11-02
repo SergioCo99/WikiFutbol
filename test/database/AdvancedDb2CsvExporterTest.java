5750a29a8a260bba2b3206bc607fbfbf5f338f7d
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-11-02 19:06:58 +0100

    se testea que se crean los ficheros con su nombre correspondiente

--- test/database/AdvancedDb2CsvExporterTest.java
+++ test/database/AdvancedDb2CsvExporterTest.java
@@ -1,5 +1,13 @@
 package database;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,19 +15,46 @@ public class AdvancedDb2CsvExporterTest {
 
 	static AdvancedDb2CsvExporter exporter = new AdvancedDb2CsvExporter();
 
+	String table = "jugador";
+
+	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+	String dateTimeInfo = dateFormat.format(new Date());
+
 	@Before
 	public void setUp() throws Exception {
 		exporter = new AdvancedDb2CsvExporter();
 	}
 
-	@Test // SIN HACER
+	@Test
 	public void testExport() {
-		String table = "pais";
 		try {
 			AdvancedDb2CsvExporter.export(table);
+
+			String fileName = table + "_ExportFromDataBase_" + dateTimeInfo + ".csv";
+			File file = new File(System.getProperty("user.home") + "/Downloads/" + fileName);
+			assertTrue(file.exists());
+
+			file.delete();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	@Test
+	public void classesExport() {
+		try {
+			AdvancedDb2CsvExporter.classesExport(table);
+
+			String fileName = table + "_ExportFromClasses_" + dateTimeInfo + ".csv";
+			File file = new File(System.getProperty("user.home") + "/Downloads/" + fileName);
+			assertTrue(file.exists());
+
+			file.delete();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
