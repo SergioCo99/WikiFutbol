4930f4c5bf8f607bef3a5abb38297fdb02d7b056
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-11-25 14:36:28 +0100

    .

--- src/ventanas/VentanaRegistro.java
+++ src/ventanas/VentanaRegistro.java
@@ -137,7 +137,7 @@ public class VentanaRegistro extends JFrame {
 								mainPackage.MainWikiFutbol.loggerGeneral.log(Level.INFO, e1.toString());
 								e1.printStackTrace();
 							}
-							utils.PropertiesMetodos.setProp(txtCorreo.getText(), txtPassword.getPassword().toString()); // CORREGIRLO
+							utils.PropertiesMetodos.setProp(txtCorreo.getText(), txtPassword.getPassword().toString()); //String.valueOf(txtPassword.getPassword()); // CORREGIRLO
 
 							dispose();
 							VentanaPrincipal VP = null;
